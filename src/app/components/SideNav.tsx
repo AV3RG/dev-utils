7d22d0c7e452cf497fe57323cbb540a93f40d509
RG <goyalrohan2020@gmail.com>
2024-11-03 00:36:30 +0530

    feat: move utils data to constant

--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -2,33 +2,46 @@
 
 import Link from "next/link";
 import {cn} from "@/lib/utils";
-import {Loader, WholeWord} from "lucide-react";
+import {Loader, LucideIcon} from "lucide-react";
 import React from "react";
 import {usePathname} from "next/navigation";
+import utils from "@/const/utils";
+
+function SideItem(props: {
+    pathName: string,
+    icon: LucideIcon,
+    displayName: string
+}) {
+
+    const currentPath = usePathname()
+
+    const isActive = (path: string) => {
+        return currentPath === path
+    }
+
+    return <Link
+        href={props.pathName}
+        className={cn(
+            "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary",
+            {
+                "bg-muted": isActive(props.pathName),
+                "text-muted-foreground": !isActive(props.pathName),
+            }
+        )}
+    >
+        <props.icon className={"h-4 w-4"} />
+        {props.displayName}
+    </Link>
+}
 
 export default function SideNav() {
 
-    const pathName = usePathname()
-
-    const isActive = (path: string) => {
-        return pathName === path
-    }
-
     return <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
 
-        <Link
-            href="/strinfo"
-            className={cn(
-                "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary",
-                {
-                    "bg-muted": isActive("/strinfo"),
-                    "text-muted-foreground": !isActive("/strinfo"),
-                }
-            )}
-        >
-            <WholeWord className="h-4 w-4"/>
-            String Info
-        </Link>
+        {utils.map((utilData) => {
+            return <SideItem key={utilData.pathName} pathName={utilData.pathName} icon={utilData.icon} displayName={utilData.displayName} />
+        })}
+
         <Link
             href="#"
             className={cn(
