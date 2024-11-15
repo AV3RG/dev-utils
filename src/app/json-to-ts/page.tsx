e85b5bc61276ed07e5cf1b18ce5d43492f05eabf
RG <goyalrohan2020@gmail.com>
2024-11-15 12:43:43 +0530

    feat(json-to-ts): initial

---
+++ src/app/json-to-ts/page.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import React, {useState, useEffect, useCallback} from 'react'
+import { Textarea } from "@/components/shadcn/ui/textarea"
+import { Alert, AlertDescription } from "@/components/shadcn/ui/alert"
+import {AlertCircle, Clipboard, Upload} from "lucide-react"
+import { Button } from "@/components/shadcn/ui/button"
+import debounce from 'lodash.debounce'
+
+//TODO: Option to select between interfaces and types
+//TODO: Option to select between single or multiple types
+//TODO: Option to select between readonly or mutable types
+//TODO: Option to select between optional or required types
+//TODO: Handle multiple types in a single array (e.g. [1, "string", true])
+
+export default function JsonToTs() {
+    const [input, setInput] = useState('')
+    const [output, setOutput] = useState('')
+    const [error, setError] = useState('')
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const convertJsonToTs = (obj: any, indent = 0): string => {
+        if (Array.isArray(obj)) {
+            const itemType = obj.length > 0 ? convertJsonToTs(obj[0]) : 'any'
+            return `Array<${itemType}>`
+        } else if (typeof obj === 'object' && obj !== null) {
+            let output = '{\n'
+            for (const [key, value] of Object.entries(obj)) {
+                output += `${'  '.repeat(indent + 1)}${key}: ${convertJsonToTs(value, indent + 1)};\n`
+            }
+            output += `${'  '.repeat(indent)}}`
+            return output
+        } else {
+            return typeof obj
+        }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const handleConvert = useCallback(debounce(() => {
+        try {
+            const jsonObj = JSON.parse(input)
+            const tsType = `type GeneratedType = ${convertJsonToTs(jsonObj)}`
+            setOutput(tsType)
+            setError('')
+        } catch (err) {
+            setError('Invalid JSON input. Please check your JSON and try again.')
+            setOutput('')
+            console.error('Error while converting JSON to TS:', err)
+        }
+    }), [convertJsonToTs, input])
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const value = e.target.value
+        setInput(value)
+    }
+
+    useEffect(() => {
+        if (input) {
+            handleConvert()
+        }
+    }, [handleConvert, input])
+
+    useEffect(() => {
+        console.log(input, output, error)
+    }, [input, output, error]);
+
+    return (
+        <div className="container mx-auto p-4">
+            <div className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl font-bold">TS Type/Interface Extractor</h1>
+            </div>
+            <div className="flex items-center justify-between mb-4">
+                <div>
+                    {error && (
+                        <Alert variant="destructive" className={"p-2 flex items-center justify-center [&>svg]:absolute [&>svg]:left-3 [&>svg]:top-2"}>
+                            <AlertCircle className="h-4 w-4" />
+                            <AlertDescription>{error}</AlertDescription>
+                        </Alert>
+                    )}
+                </div>
+                <div className={"flex items-center justify-end gap-x-4"}>
+                    <Button variant="outline" onClick={() => document.getElementById('file-upload')?.click()}>
+                        <Upload className="mr-2 h-4 w-4"/> Upload File
+                    </Button>
+                    <input
+                        id="file-upload"
+                        type="file"
+                        onChange={() => {}}
+                        className="hidden"
+                        accept=".*/*"
+                    />
+                    <Button variant="outline" onClick={() => setInput("")}>Clear</Button>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                    <label htmlFor="json-input" className="block text-sm font-medium text-gray-700 mb-2">
+                        Input
+                    </label>
+                    <Textarea
+                        id="json-input"
+                        value={input}
+                        onChange={handleInputChange}
+                        placeholder="Type or paste your JSON here..."
+                        className="min-h-[300px] h-full"
+                    />
+                </div>
+                <div>
+                    <h2 className="text-sm font-medium text-gray-700 mb-2">Extracted Type</h2>
+                    <pre className="bg-gray-100 p-4 rounded-md overflow-auto h-full min-h-[300px] relative">
+                            <Button
+                                className={"absolute right-4 top-4 p-2 w-fit h-fit"}
+                                size={"icon"}
+                                onClick={async () => {
+                                    try {
+                                        await navigator.clipboard.writeText(output)
+                                    } catch (e) {
+                                        console.error("Error while copying to clipboard:", e)
+                                    }
+                                }}
+                            >
+                                <Clipboard className={"h-4 w-4"} />
+                            </Button>
+                            <code>{output}</code>
+                        </pre>
+                </div>
+            </div>
+        </div>
+    )
+}
