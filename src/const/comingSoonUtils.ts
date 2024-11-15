3fa7063ed8dd848d6f962566bb2870fcfbc0ca51
RG <76780190+AV3RG@users.noreply.github.com>
2024-11-15 21:04:53 +0530

    fix(home): utils const files sync (#9)

---
+++ src/const/comingSoonUtils.ts
@@ -0,0 +1,13 @@
+const comingSoonUtils: {
+    displayName: string;
+    cardContent: string;
+    cardDescription: string;
+}[] = [
+    {
+        displayName: "Color Converter",
+        cardDescription: "Convert between different color formats.",
+        cardContent: "HEX, RGB, HSL, and more. (Coming Soon)"
+    }
+]
+
+export default comingSoonUtils;
--- src/const/utils.ts
+++ src/const/utils.ts
@@ -3,22 +3,30 @@ import {FileJson2, LucideIcon, Sparkles, WholeWord} from "lucide-react";
 const utils: {
   pathName: string,
   icon: LucideIcon,
-  displayName: string
+  displayName: string,
+  cardDescription: string,
+  cardContent: string
 }[] = [
   {
     pathName: "/strinfo",
     icon: WholeWord,
-    displayName: "String info"
+    displayName: "String info",
+    cardDescription: "Get all the info about a string that you will need.",
+    cardContent: "Word count, Byte Count, Line Endings and much more."
   },
   {
     pathName: "/json-pretty",
     icon: Sparkles,
-    displayName: "Json Prettifier"
+    displayName: "Json Prettifier",
+    cardDescription: "Prettify your JSON with ease.",
+    cardContent: "Make your JSON more readable and easy to understand."
   },
   {
     pathName: "/json-to-ts",
     icon: FileJson2,
-    displayName: "JSON to TS"
+    displayName: "JSON to TS",
+    cardDescription: "Convert JSON to TypeScript interfaces.",
+    cardContent: "Generate TypeScript interfaces from your JSON data."
   }
 ]
 
