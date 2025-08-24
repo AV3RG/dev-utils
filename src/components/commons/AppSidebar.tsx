9fa7f43f2860b9b49cb83773535a45682ce2b5cb
RG <76780190+AV3RG@users.noreply.github.com>
2025-08-25 00:25:52 +0530

    ref: sidebar rework (#25)
    * ref: sidebar rework
    
    * chore(version-bump): 0.8.0

---
+++ src/components/commons/AppSidebar.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Wrench, Loader } from "lucide-react";
+import utils from "@/const/utils";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from "@/components/shadcn/ui/sidebar";
+
+export function AppSidebar() {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar variant="inset">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Wrench className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">Dev Utils</span>
+                  <span className="truncate text-xs">Developer Utilities</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {utils.map((util) => (
+                <SidebarMenuItem key={util.pathName}>
+                  <SidebarMenuButton asChild isActive={pathname === util.pathName}>
+                    <Link href={util.pathName}>
+                      <util.icon />
+                      <span>{util.displayName}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="#" className="cursor-default">
+                <Loader />
+                <span>More Coming Soon...</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
